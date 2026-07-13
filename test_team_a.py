ad2eb01d42e5dc42e044631115e872ea69e65960
MatthewR863 <matthewreynolds085@gmail.com>
2026-07-13 19:34:27 +0000

    US_BANK_NUMBER positive and negative test cases added

--- test_team_a.py
+++ test_team_a.py
@@ -17,6 +17,15 @@ class TestTeam_a(unittest.TestCase):
 
     def test_us_bank_number(self):
         """Test US_BANK_NUMBER functionality"""
+    # positive test case
+        result = 8 <= len("12345678901234567") <= 17 and "12345678901234567".isdigit()
+        print(f"Result: {result}")
+        assert result
+
+    # negative test case
+        result = 8 <= len("1234567") <= 17 and "1234567".isdigit()
+        print(f"Result: {result}")
+        assert not result
 
     def test_us_driver_license(self):
         """Test US_DRIVER_LICENSE functionality"""
