9b1fe58d9e44f2fc7b1fdda9b73c82b303d2751d
Amaka Okoro <aaokoro@aggies.ncat.edu>
2026-07-16 13:14:24 -0700

    IT_FISCAL_CODE positive and negative test cases

--- test_team_moses.py
+++ test_team_moses.py
@@ -17,36 +17,20 @@ class TestTeam_moses(unittest.TestCase):
 
     def test_it_fiscal_code(self):
         """Test IT_FISCAL_CODE functionality"""
-        valid_codes = [
-            "VRNGNY07D68C351V",
-            "RCCMNL83S18D969H",
-            "MRNMIA02E45L219X",
-        ]
+        # positive test case
+        test_str = 'my fiscal code is VRNGNY07D68C351V'
+        result = analyze_text(test_str, ['IT_FISCAL_CODE'])
+        self.assertEqual(result[0].entity_type, 'IT_FISCAL_CODE')
 
-        for code in valid_codes:
-            with self.subTest(code=code):
-                text = f"Codice fiscale: {code}"
-                results = analyze_text(text, ["IT_FISCAL_CODE"])
+        # negative test case
+        test_str = 'my fiscal code is 1234567890123456'
+        result = analyze_text(test_str, ['IT_FISCAL_CODE'])
+        self.assertFalse(result)
 
-                self.assertEqual(len(results), 1)
-                self.assertEqual(results[0].entity_type, "IT_FISCAL_CODE")
-                self.assertEqual(text[results[0].start:results[0].end], code)
-                self.assertGreater(results[0].score, 0)
-
-        invalid_codes = [
-            "1234567890123456",
-            "ABCDEFGHIJKLMNO",
-            "ABC123456789XYZ",
-            "VRNGNY07D68C351K",
-        ]
-
-        for code in invalid_codes:
-            with self.subTest(code=code):
-                results = analyze_text(
-                    f"Codice fiscale: {code}",
-                    ["IT_FISCAL_CODE"],
-                )
-                self.assertEqual(results, [])
+        # negative test case - correct format but invalid checksum
+        test_str = 'my fiscal code is VRNGNY07D68C351K'
+        result = analyze_text(test_str, ['IT_FISCAL_CODE'])
+        self.assertFalse(result)
 
     def test_it_identity_card(self):
         """Test IT_IDENTITY_CARD functionality"""
